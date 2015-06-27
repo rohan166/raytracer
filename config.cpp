7e2bae762995d86c339d7d7a9457e3a7b8030f23
Rohan Rangray <rrangray@ucsd.edu>
2015-06-27 16:56:11 -0700

    Added a very crude config.cpp

---
+++ config.cpp
@@ -0,0 +1,33 @@
+//
+// Created by rray on 6/27/15.
+//
+
+#include <stdio.h>
+#include <string>
+#include "config.h"
+#include "scene.h"
+#include "primitives/primitive.h"
+#include "primitives/sphere.h"
+
+#define NUM_OBJECTS 2
+
+using namespace std;
+
+Scene * parse_config (Config *config) {
+    Primitive *shapes[NUM_OBJECTS];
+    unsigned int height, width, counter = 0;
+    FILE *in_config = fopen("config");
+    string directive(100, '\0'), value(100, '\0');
+
+    while (fscanf(in_config, "%s: %s", &directive[0], &value[0])) {
+        if (directive.compare("height"))
+            height = (unsigned int) stoi(directive, nullptr);
+        else if (directive.compare("width"))
+            width = (unsigned int) stoi(directive, nullptr);
+        else if (directive.compare("camera"))
+            shapes[counter++] = new Camera(value);
+        else if (directive.compare("sphere"))
+            shapes[counter++] = new Sphere(value);
+    }
+    return new Scene(height, width, shapes);
+}
---
+++ config.h
@@ -0,0 +1,8 @@
+//
+// Created by rray on 6/27/15.
+//
+
+#ifndef RAYTRACER_CONFIG_H
+#define RAYTRACER_CONFIG_H
+
+#endif //RAYTRACER_CONFIG_H
