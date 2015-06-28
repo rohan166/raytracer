c94ec7e441da7db407d375c251ecd7b1bca361f4
Rohan Rangray <rrangray@ucsd.edu>
2015-06-27 19:00:20 -0700

    Changed Point3 to include both + and +=

--- Point3.cpp
+++ Point3.cpp
@@ -3,11 +3,17 @@
 
 Point3::Point3(float x, float y, float z): Triplet(x, y, z) {}
 
-Vector3 Point3::operator-(Point3 &other) {
+Vector3* Point3::operator-(Point3 &other) {
     return new Vector3(this, other);
 }
 
-Point3 Point3::operator+(Vector3 &other) {
+Point3* Point3::operator+(Vector3 &other) {
+    Point3 *p = new Point3(this);
+    *p += other;
+    return p;
+}
+
+void Point3::operator+=(Vector3 &other) {
     v[0] += other.magnitude*other.first();
     v[1] += other.magnitude*other.second();
     v[2] += other.magnitude*other.third();
--- Point3.h
+++ Point3.h
@@ -7,8 +7,9 @@
 class Point3 : public Triplet {
 public:
     Point3 (float x, float y, float z);
-    Vector3 operator-(Point3 &other);
-    Point3 operator+(Vector3 &other);
+    Vector3* operator-(Point3 &other);
+    void operator+=(Vector3 &other);
+    Point3* operator+(Vector3 &other);
 };
 
 
