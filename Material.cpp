8c0d0aacf602c41bdb3ae555775d05fa2978c518
Andrew Buss <abuss@ucsd.edu>
2015-06-30 23:43:45 -0700

    for diffuse material, check if the cosine is negative and ignore it if so

--- Material.cpp
+++ Material.cpp
@@ -9,6 +9,8 @@ Color Material::computeColor(const Intersection& intersection, const Scene& scen
         // L.N == |N||L|*cos(angle), and since L and N are normalized below, we can compute the
         // cosine of the angle between the two vectors using the dot product
         double cosine = light_vector.normalized().dot(intersection.normal.normalized());
+        // if the angle is more than 90 degrees, it shouldn't affect the result
+        if (cosine <= 0) continue;
         color = color + diffuse_color * cosine;
     }
     return color;
